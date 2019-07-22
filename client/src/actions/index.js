c3d18fd071298b5c5ec21f4510ff1b2c1da94b02
angelikaSemeniuk <Anzhelika@Maxs-MacBook-Pro.local>
2019-07-22 09:17:20 +0300

    Implement modal window for delete component

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,5 @@
-import streams from '../api/streams';
-import history from '../history';
+import streams from "../api/streams";
+import history from "../history";
 import {
   SIGN_IN,
   SIGN_OUT,
@@ -8,32 +8,32 @@ import {
   FETCH_STREAMS,
   DELETE_STREAM,
   EDIT_STREAM
-} from './types';
+} from "./types";
 
-export const signIn = (userId) => {
+export const signIn = userId => {
   return { type: SIGN_IN, payload: userId };
 };
 
 export const signOut = () => {
-  return { type: SIGN_OUT }
+  return { type: SIGN_OUT };
 };
 
 export const createStream = formValues => async (dispatch, getState) => {
-  const {userId} = getState().auth;
-  const response = await streams.post('/streams', { ...formValues, userId} );
+  const { userId } = getState().auth;
+  const response = await streams.post("/streams", { ...formValues, userId });
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
   //programmatic navigation to get the user back to the root route
-  history.push('/');
+  history.push("/");
 };
 
 export const fetchStreams = () => async dispatch => {
-  const response = await streams.get('/streams');
+  const response = await streams.get("/streams");
 
   dispatch({ type: FETCH_STREAMS, payload: response.data });
 };
 
-export const fetchStream = (streamId) => async dispatch => {
+export const fetchStream = streamId => async dispatch => {
   const response = await streams.get(`/streams/${streamId}`);
 
   dispatch({ type: FETCH_STREAM, payload: response.data });
@@ -43,11 +43,12 @@ export const editStream = (streamId, formValues) => async dispatch => {
   const response = await streams.patch(`/streams/${streamId}`, formValues);
 
   dispatch({ type: EDIT_STREAM, payload: response.data });
-  history.push('/');
+  history.push("/");
 };
 
-export const deleteStream = (streamId) => async dispatch => {
+export const deleteStream = streamId => async dispatch => {
   await streams.delete(`/streams/${streamId}`);
 
   dispatch({ type: DELETE_STREAM, payload: streamId });
+  history.push("/");
 };
--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,9 +1,56 @@
-import React from 'react';
+import React from "react";
+import Modal from "../Modal";
+import { Link } from "react-router-dom";
+import history from "../../history";
+import { connect } from "react-redux";
+import { fetchStream, deleteStream } from "../../actions";
 
-const StreamDelete = () => {
-    return(
-        <div>StreamDelete</div>
+class StreamDelete extends React.Component {
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchStream(id);
+  }
+
+  renderContent() {
+    if (!this.props.stream) {
+      return "Are you sure you want to delete this stream?";
+    }
+    return `Are you sure you want to delete this stream with title: ${
+      this.props.stream.title
+    } ?`;
+  }
+  render() {
+    const actions = (
+      <>
+        <div
+          onClick={() => this.props.deleteStream(this.props.stream.id)}
+          class="ui button negative"
+        >
+          Delete
+        </div>
+        <Link to="/" class="ui button">
+          Cancel
+        </Link>
+      </>
     );
+    return (
+      <Modal
+        title="Delete Streams"
+        content={this.renderContent()}
+        actions={actions}
+        onDismiss={() => history.push("/")}
+      />
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    stream: state.streams[ownProps.match.params.id]
+  };
 };
 
-export default StreamDelete;
+export default connect(
+  mapStateToProps,
+  { fetchStream, deleteStream }
+)(StreamDelete);
